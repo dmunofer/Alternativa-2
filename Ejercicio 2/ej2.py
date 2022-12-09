9abc652c0926cef050859434661b6188d6255ad9
dmunofer <diego.fmz@hotmail.com>
2022-12-10 00:12:40 +0100

    cambios

--- Ejercicio 2/ej2.py
+++ Ejercicio 2/ej2.py
@@ -1,1 +1,12 @@
 import math
+
+def sacar_a(formula):
+    i=0
+    while i in range(0, len(formula)):
+        if type(formula[i])!=str:
+            i+=1
+        else:
+            a=formula[0:i]
+    return a
+
+
