26923407164974b0a22474535e006b84e3178278
dmunofer <diego.fmz@hotmail.com>
2022-12-09 23:16:44 +0100

    cambios

--- Ejercicio 1/ej1.py
+++ Ejercicio 1/ej1.py
@@ -9,3 +9,21 @@ sudoku = [
     [1,2,0,0,0,7,4,0,0],
     [0,4,9,2,0,6,0,0,7]
 ]
+
+def resolver(bo):
+    find = encontrar_vacia(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if valido(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if resolver(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
