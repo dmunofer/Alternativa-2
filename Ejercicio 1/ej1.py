97cbf984c1e763fc2cc1038667bb3590139b025f
dmunofer <diego.fmz@hotmail.com>
2022-12-09 23:19:23 +0100

    cambios

--- Ejercicio 1/ej1.py
+++ Ejercicio 1/ej1.py
@@ -49,3 +49,17 @@ def valido(sudoku, num, pos):
                 return False
 
     return True
+
+def mostrar_sudoku(sudoku):
+    for i in range(len(sudoku)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - - - ")
+
+        for j in range(len(sudoku[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(sudoku[i][j])
+            else:
+                print(str(sudoku[i][j]) + " ", end="")
