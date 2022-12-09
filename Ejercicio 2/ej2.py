e451fc1a28b170cd5d5c8806c7281b00a791ee2d
dmunofer <diego.fmz@hotmail.com>
2022-12-10 00:20:00 +0100

    cambios

--- Ejercicio 2/ej2.py
+++ Ejercicio 2/ej2.py
@@ -7,6 +7,16 @@ def sacar_a(formula):
             i+=1
         else:
             a=formula[0:i]
-    return a
+    return a,i
+
+def sacar_b(formula):
+    a,i=sacar_a(formula)
+    cont=0
+    while i+1 in range(0, len(formula)):
+        if formula[i+1]!=')':
+            cont+=1
+        else:
+            b=formula[1+1:cont]
+    return b
 
 
