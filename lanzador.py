46d0ab4c95b46bb1063ce9c870740f5365715f14
dmunofer <diego.fmz@hotmail.com>
2022-12-10 01:06:12 +0100

    cambios

---
+++ lanzador.py
@@ -0,0 +1,14 @@
+import sys
+
+sys.path.insert(0,"/Users/smite/Documents/GitHub/Alternativa2/Ejercicio1/ej1")
+sys.path.insert(0,"/Users/smite/Documents/GitHub/Alternativa2/Ejercicio2/ej2")
+sys.path.insert(0,"/Users/smite/Documents/GitHub/Alternativa2/Ejercicio3/ej3")
+sys.path.insert(0,"/Users/smite/Documents/GitHub/Alternativa2/Ejercicio2/ej4")
+sys.path.insert(0,"/Users/smite/Documents/GitHub/Alternativa2/Ejercicio3/ej5")
+
+
+from Ejercicio1 import ej1
+from Ejercicio2 import ej2
+from Ejercicio3 import ej3
+from Ejercicio4 import ej4
+from Ejercicio5 import ej5
